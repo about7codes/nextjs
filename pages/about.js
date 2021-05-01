c42c9fd64d747bae30933a88bc1e75e6b9b7f1ec
about7code <sanaullahhaifz@gmail.com>
2021-05-01 16:48:16 +0400

    init

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+const About = () => {
+    return ( 
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas"/>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, veritatis! Omnis temporibus magnam nobis adipisci delectus deleniti architecto aliquid eligendi asperiores corrupti. Ipsum cumque voluptate repellendus in sed! Recusandae, placeat.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, veritatis! Omnis temporibus magnam nobis adipisci delectus deleniti architecto aliquid eligendi asperiores corrupti. Ipsum cumque voluptate repellendus in sed! Recusandae, placeat.</p>
+            </div>
+        </>
+     );
+}
+ 
+export default About;
